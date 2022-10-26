b8ffb89af7afc95a70ca667454560519935fb3db
fallah rasool <fallah.rasol@gmail.com>
2022-10-26 18:56:05 +0330

    create Post routes and create store fu PostController and create newPost fu ModelPostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class PostController extends ApiController
 {
@@ -11,7 +13,32 @@ class PostController extends ApiController
     public function index(){
 
         return $this->successResponse(200,Post::all(),'okConnect');
-        
+
+    }
+
+
+    public function store(Request $request, Post $post){
+
+
+        //validate
+        $validate  = Validator::make($request->all(),[
+            'title'=>'required|string',
+            'slug'=>'required|string',
+            'image'=>'required|string',
+            'content'=>'required|string',
+            'user_id'=>'required',
+        ]);
+
+        if($validate->fails()){
+            return $this->errorResponse(400, $validate->messages());
+        }
+
+        $post->newPost($request);
+
+        $dataResponse = $post->orderBy('id','desc')->first();
+
+        return $this->successResponse(200,$dataResponse,'post create successFully');
+
     }
 
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $guarded=[];
+
+
+
+    public function newPost(Request $request){
+        
+        Post::query()->create([
+            'title'=>$request->title,
+            'slug'=>$request->slug,
+            'image'=>$request->image,
+            'content'=>$request->get('content'),
+            'user_id'=>1
+        ]);
+    }
+
 }
