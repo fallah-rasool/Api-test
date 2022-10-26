d470992611301f86767d6e407c07715c73a0f583
fallah rasool <fallah.rasol@gmail.com>
2022-10-27 02:59:16 +0330

    to deactivate wrao Global

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        JsonResource::wrap('rasool');
+       // JsonResource::wrap('rasool');
+
+       JsonResource::withoutWrapping();
     }
 }
