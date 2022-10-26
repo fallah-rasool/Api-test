411fb510c2da46c63b45fd88370eda13a7ad2457
fallah rasool <fallah.rasol@gmail.com>
2022-10-26 17:58:37 +0330

    create Traits >> ApiRescponse

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiRescponse;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+
+    use ApiRescponse;
 }
---
+++ app/Traits/ApiRescponse.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Traits;
+trait ApiRescponse
+{
+    protected function successResponse($code ,$data,$message = null){
+        return response()->json([
+            'status'=> 'success',
+            'message'=>$message,
+            'data'=>$data,
+        ],$code);
+    }
+    protected function errorResponse($code,$message = null){
+        return response()->json([
+            'status'=> 'error',
+            'message'=>$message,
+        ],$code);
+    }
+}
