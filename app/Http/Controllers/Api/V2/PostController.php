af0c04a9c20a994dbdedaa5943201b667066971e
fallah rasool <fallah.rasol@gmail.com>
2022-10-27 10:56:38 +0330

    Versioning

---
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\PostResource;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends ApiController
+{
+    public function index(){
+
+        //  $posts = Post::all();
+
+        //  return new PostCollection($posts);
+
+
+      //    return $this->successResponse(200,PostResource::collection($posts),'okConnect');
+
+          // return $this->successResponse(200,new PostResource($posts)  ,'okConnect');
+
+      //    return $this->successResponse(200,Post::all(),'okConnect');
+
+
+     // $posts = Post::paginate(3);
+
+      // return $this->successResponse(200 , [
+
+      //  'posts' =>  PostResource::collection($posts),
+
+      //  'links' => PostResource::collection($posts)->response()->getData()->links,
+
+      //  'meta' => PostResource::collection($posts)->response()->getData()->meta,
+      // ],'okConnect');
+
+      $posts = Post::paginate(3);
+      
+      return PostResource::collection($posts);
+
+
+      }
+
+
+      public function store(Request $request, Post $post){
+
+
+          //validate
+          $validate  = Validator::make($request->all(),[
+              'title'=>'required|string',
+              'slug'=>'required|string',
+              'image'=>'required|image',
+              'content'=>'required|string',
+              'user_id'=>'required',
+          ]);
+
+          if($validate->fails()){
+              return $this->errorResponse(400, $validate->messages());
+          }
+
+          $post->newPost($request);
+
+          $dataResponse = $post->orderBy('id','desc')->first();
+
+          return $this->successResponse(200,$dataResponse,'post create successFully');
+
+      }
+
+      public function show(Post $post){
+
+        //  $dataResponse =  new PostResource($post);
+
+        //  return $this->successResponse(200,$dataResponse,'getOk');
+
+       // return new PostResource($post);
+
+       $dataResponse =  new PostResource($post);
+
+       return $dataResponse->additional([
+            'food' =>[
+                'slug'=>$post->slug,
+            ]
+        ]);
+
+      }
+
+
+      public function update(Request $request ,Post $post){
+
+
+          //validate
+          $validate  = Validator::make($request->all(),[
+             'title'=>'required|string',
+             'slug'=>'required|string',
+             'image'=>'image',
+             'content'=>'required|string',
+             'user_id'=>'required',
+         ]);
+         if($validate->fails()){
+             return $this->errorResponse(400, $validate->messages());
+         }
+
+          //  updataPost
+
+          $post->updataPost($request,$post);
+
+          return $this->successResponse(200,$post,'updata Post successFully');
+
+
+      }
+
+
+      public function destroy(Post $post){
+
+
+          $post->deletePost($post);
+
+           return $this->successResponse(200,$post,'deleted Post successFully');
+
+       }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+use App\Http\Controllers\Api\V1\PostController as V1PostController;
+use App\Http\Controllers\Api\V2\PostController as V2PostController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,14 +28,53 @@ use App\Http\Controllers\UserController;
 
 
 
-Route::get('posts',[PostController::class,'index'] );
+// Route::get('posts',[PostController::class,'index'] );
 
-Route::post('posts',[PostController::class,'store'] );
+// Route::post('posts',[PostController::class,'store'] );
 
-Route::put('posts/{post}',[PostController::class,'update']);
+// Route::put('posts/{post}',[PostController::class,'update']);
 
-Route::delete('posts/{post}',[PostController::class,'destroy']);
+// Route::delete('posts/{post}',[PostController::class,'destroy']);
 
-Route::get('posts/{post}',[PostController::class,'show']);
+// Route::get('posts/{post}',[PostController::class,'show']);
 
-Route::apiResource('users',UserController::class);
+// Route::apiResource('users',UserController::class);
+
+
+
+Route::prefix('v1')->group(function () {
+
+    Route::get('posts',[V1PostController::class,'index']);
+
+    Route::get('posts/{post}',[V1PostController::class,'show']);
+
+    Route::post('posts',[V1PostController::class,'store']);
+
+    Route::put('posts/{post}',[V1PostController::class,'update']);
+
+    Route::delete('posts/{post}',[V1PostController::class,'destroy']);
+
+    Route::apiResource('users',UserController::class);
+
+});
+
+
+Route::prefix('v2')->group(function () {
+
+
+    Route::get('posts',[V2PostController::class,'index']);
+
+    Route::get('posts/{post}',[V2PostController::class,'show']);
+
+    Route::post('posts',[V2PostController::class,'store']);
+
+    Route::put('posts/{post}',[V2PostController::class,'update']);
+
+    Route::delete('posts/{post}',[V2PostController::class,'destroy']);
+
+    Route::apiResource('users',UserController::class);
+
+
+});
+
+
